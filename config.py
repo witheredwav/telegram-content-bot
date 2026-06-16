117e02ae77aff68753318467dd960e35b0aeb596
witheredwav <steam.akk1337@bk.ru>
2026-06-16 18:11:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-WORKS_LINK = os.getenv("WORKS_LINK")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "your_channel")
+
+CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/your_channel")
+
+WORKS_LINK = os.getenv("WORKS_LINK", "https://t.me/your_examples")
