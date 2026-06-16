27ec377e93c8ec8115533e27bf62a22bc7d19a13
witheredwav <steam.akk1337@bk.ru>
2026-06-17 00:00:09 +0500

    Update config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,18 +3,17 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
-
     DATABASE_URL: str
 
-    REDIS_URL: str
+    REDIS_URL: str = "redis://localhost:6379"
 
-    CHANNEL_ID: int
-    CHANNEL_USERNAME: str
+    CHANNEL_ID: str | None = None
+    CHANNEL_USERNAME: str | None = None
 
-    WORKS_CHANNEL_ID: int
-    WORKS_CHANNEL_USERNAME: str
+    WORKS_CHANNEL_ID: str | None = None
+    WORKS_CHANNEL_USERNAME: str | None = None
 
-    ADMIN_IDS: str  # можно парсить позже
+    ADMIN_IDS: str = ""
 
     class Config:
         env_file = ".env"
