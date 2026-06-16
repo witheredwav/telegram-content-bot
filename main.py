697266b86d2a031122299512e3274d8ecc7f4553
witheredwav <steam.akk1337@bk.ru>
2026-06-16 21:53:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from app.bot import bot
 from app.utils.logger import logger
 
 from app.database.migrate import run_migrations
-from app.database.session import engine
-from app.database.redis import redis
 
 from app.handlers.start import router as start_router
+from app.handlers.subscription import router as subscription_router
+from app.handlers.menu import router as menu_router
+from app.handlers.works import router as works_router
+from app.handlers.referrals import router as referrals_router
+from app.handlers.request import router as request_router
 
 
 async def main():
@@ -18,14 +21,14 @@ async def main():
 
     run_migrations()
 
-    async with engine.begin() as conn:
-        await conn.run_sync(lambda _: None)
-
-    await redis.ping()
-
     dp = Dispatcher()
 
     dp.include_router(start_router)
+    dp.include_router(subscription_router)
+    dp.include_router(menu_router)
+    dp.include_router(works_router)
+    dp.include_router(referrals_router)
+    dp.include_router(request_router)
 
     logger.info("Bot started")
 
