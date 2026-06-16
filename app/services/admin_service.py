97428c98e9f15581df218db7418164eab1e367e2
witheredwav <steam.akk1337@bk.ru>
2026-06-16 22:11:25 +0500

    Update admin_service.py

--- app/services/admin_service.py
+++ app/services/admin_service.py
@@ -4,6 +4,7 @@ from app.utils.config import settings
 class AdminService:
 
     def is_admin(self, user_id: int) -> bool:
+
         admin_ids = settings.ADMIN_IDS.split(",")
 
         return str(user_id) in admin_ids
