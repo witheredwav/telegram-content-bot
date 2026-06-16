0f32ee4778c5cdfdeddfd44744f69a36c9e49d04
witheredwav <steam.akk1337@bk.ru>
2026-06-16 22:09:34 +0500

    Update init.py

--- app/models/init.py
+++ app/models/init.py
@@ -3,3 +3,4 @@ from app.models.promo_code import PromoCode
 from app.models.request import Request
 from app.models.order import Order
 from app.models.antifraud import AntiFraudLog
+from app.models.role import UserRole
