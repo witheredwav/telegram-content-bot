12ab3b071c90801490f28bc7933e8be2faf3f385
witheredwav <steam.akk1337@bk.ru>
2026-06-16 23:41:42 +0500

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,23 +1,14 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
 from app.utils.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=False,
-
-    # 🔥 ВАЖНО ДЛЯ RAILWAY
-    pool_size=5,
-    max_overflow=10,
     pool_pre_ping=True,
-    pool_recycle=1800,
-
-    connect_args={
-        "ssl": "require"
-    },
 )
 
 async_session = async_sessionmaker(
     bind=engine,
-    class_=AsyncSession,
     expire_on_commit=False,
 )
