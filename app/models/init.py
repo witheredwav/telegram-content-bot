b73200068717698f2cc7891971b1abbd025b7f72
witheredwav <steam.akk1337@bk.ru>
2026-06-16 22:12:34 +0500

    Update init.py

--- app/models/init.py
+++ app/models/init.py
@@ -4,3 +4,4 @@ from app.models.request import Request
 from app.models.order import Order
 from app.models.antifraud import AntiFraudLog
 from app.models.role import UserRole
+from app.models.audit_log import AuditLog
