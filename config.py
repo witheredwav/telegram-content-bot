f9e834e8dd88a352160ba0e24db81786952123cf
witheredwav <steam.akk1337@bk.ru>
2026-06-16 20:28:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 
-DB_NAME = "db.sqlite3"
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+PORTFOLIO_LINK = os.getenv("PORTFOLIO_LINK")
+
+DB_NAME = "bot.db"
