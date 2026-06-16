a9575da0ba69d06bc235b5422b10e65050ec9783
witheredwav <steam.akk1337@bk.ru>
2026-06-16 13:56:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = os.getenv("@witheredoff")  # например: @mychannel
+CHANNEL_ID = os.getenv("@witheredoff")
+
+ADMIN_ID = int(os.getenv("793806918"))  # твой Telegram ID
