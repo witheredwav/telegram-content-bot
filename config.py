3b18a4854a3a37ce73bd3ad428ec6d6db47271dc
witheredwav <steam.akk1337@bk.ru>
2026-06-16 19:21:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", "793806918"))
-
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "witheredoff")
-CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/witheredoff")
-WORKS_LINK = os.getenv("WORKS_LINK", "https://t.me/witheredwav")
+DB_NAME = "db.sqlite3"
