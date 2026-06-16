e49c4db19eec5c0f0f4bf23a93fbdb5ffdd844ae
witheredwav <steam.akk1337@bk.ru>
2026-06-16 17:12:19 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,18 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-CHANNEL = "https://t.me/your_channel"
+CHANNEL_LINK = "https://t.me/witheredoff"
+WORKS_LINK = "https://t.me/witheredwave"
 
 
 def start_kb():
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📢 Подписаться", url=CHANNEL)],
+        [InlineKeyboardButton(text="📢 Подписаться", url=CHANNEL_LINK)],
         [InlineKeyboardButton(text="🔍 Проверить подписку", callback_data="check")]
+    ])
+
+
+def menu_kb():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📂 Примеры работ", url=WORKS_LINK)],
+        [InlineKeyboardButton(text="🔑 Ввести код", callback_data="code")]
     ])
