0d676f209d92b9a30bbb268f4539833db0f927b8
witheredwav <steam.akk1337@bk.ru>
2026-06-16 18:31:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "793806918"))
 
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "witheredoff")
-
 CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/witheredoff")
-
 WORKS_LINK = os.getenv("WORKS_LINK", "https://t.me/witheredwav")
