77e75cbbe9b553baa6fd89fd49575e6747312de4
witheredwav <steam.akk1337@bk.ru>
2026-06-16 17:45:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 from db import init
 from handlers import router
 
+
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
 
 async def main():
     await init()
@@ -16,6 +19,7 @@ async def main():
     print("BOT STARTED")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
