3d8acba9775492f0f64ee08eaedd353a5856b00f
witheredwav <steam.akk1337@bk.ru>
2026-06-16 16:54:53 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,19 +7,18 @@ from keyboards import start_keyboard
 
 router = Router()
 
+
 @router.message(CommandStart())
 async def start_command(message: Message):
 
+    await add_user(
+        message.from_user.id,
+        message.from_user.username,
+        message.from_user.first_name
+    )
 
-await add_user(
-    telegram_id=message.from_user.id,
-    username=message.from_user.username,
-    first_name=message.from_user.first_name
-)
-
-await message.answer(
-    "👋 Добро пожаловать!\n\n"
-    "Для получения доступа необходимо подписаться на канал.",
-    reply_markup=start_keyboard()
-)
-
+    await message.answer(
+        "👋 Добро пожаловать!\n\n"
+        "Подпишитесь на канал, чтобы получить доступ.",
+        reply_markup=start_keyboard()
+    )
