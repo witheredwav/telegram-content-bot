7dcc1ce01eed8e28c7ed34003da41137796da76f
witheredwav <steam.akk1337@bk.ru>
2026-06-16 21:38:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import asyncio
 
 from aiogram import Dispatcher
+from sqlalchemy import text
 
 from app.bot import bot
+
+from app.database.session import engine
+from app.database.redis import redis
+
 from app.utils.logger import logger
 
 
+async def check_postgres() -> None:
+    async with engine.begin() as connection:
+        await connection.execute(
+            text("SELECT 1")
+        )
+
+    logger.info("PostgreSQL connected")
+
+
+async def check_redis() -> None:
+    await redis.ping()
+
+    logger.info("Redis connected")
+
+
 async def main() -> None:
+
     logger.info("Starting bot...")
 
+    await check_postgres()
+    await check_redis()
+
     dp = Dispatcher()
+
+    logger.info("Bot started")
 
     await dp.start_polling(bot)
 
