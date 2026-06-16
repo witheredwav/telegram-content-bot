fe5efa4b2bb7057a94d341565f385693254829f2
witheredwav <steam.akk1337@bk.ru>
2026-06-16 23:42:25 +0500

    Update migrate.py

--- app/database/migrate.py
+++ app/database/migrate.py
@@ -1,19 +1,6 @@
-import asyncio
-from alembic import command
-from alembic.config import Config
 from app.database.session import engine
 
 
 async def run_migrations():
-    # 🔥 даём Railway Postgres время подняться
-    await asyncio.sleep(5)
-
-    try:
-        alembic_cfg = Config("alembic.ini")
-
-        # синхронный вызов внутри async окружения
-        command.upgrade(alembic_cfg, "head")
-
-    except Exception as e:
-        print(f"[MIGRATIONS ERROR] {e}")
-        # не валим весь бот
+    # временно отключаем миграции чтобы бот запускался
+    return
