1da50778aaaa3fe470964a425ac7725918254ad3
witheredwav <steam.akk1337@bk.ru>
2026-06-17 00:02:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-
-from app.utils.config import settings
-from app.handlers.start import router as start_router
+from app.bot import bot, dp
+from app.handlers import start  # noqa
 from app.database.migrate import run_migrations
-
-bot = Bot(token=settings.BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(start_router)
 
 
 async def main():
     await run_migrations()
-
-    print("Bot started")
-
     await dp.start_polling(bot)
 
 
