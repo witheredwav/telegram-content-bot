32d1d1f8c776d11f522024a21d22cd2dc08f4470
witheredwav <steam.akk1337@bk.ru>
2026-06-16 21:52:45 +0500

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -8,6 +8,8 @@ from app.services.referral_service import ReferralService
 from app.services.subscription_service import SubscriptionService
 
 from app.keyboards.subscription import subscription_keyboard
+from app.keyboards.main_menu import main_menu_keyboard
+
 from app.bot import bot
 
 router = Router()
@@ -36,24 +38,19 @@ async def start_handler(message: Message):
             referral_code=referral_code
         )
 
-    # 🔥 проверка подписки
     is_sub = await subscription_service.is_subscribed(
         bot,
         message.from_user.id
     )
 
     if not is_sub:
-
         await message.answer(
-            "👋 Привет!\n\n"
-            "Чтобы пользоваться ботом, подпишись на канал 👇",
+            "👋 Привет!\n\nПодпишись на канал 👇",
             reply_markup=subscription_keyboard()
         )
         return
 
     await message.answer(
-        f"👋 Добро пожаловать!\n\n"
-        f"🎁 Твой код: <b>{user.referral_code}</b>\n"
-        f"👥 Рефералов: {user.referrals_count}\n"
-        f"💰 Скидка: {user.discount_percent}%"
+        "📋 Добро пожаловать в меню!",
+        reply_markup=main_menu_keyboard()
     )
