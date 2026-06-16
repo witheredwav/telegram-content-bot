85b156cba02475193403ff5e63b7707e7b1a7638
witheredwav <steam.akk1337@bk.ru>
2026-06-16 23:59:47 +0500

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,14 +1,20 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.utils.config import settings
 
+# ВАЖНО: asyncpg драйвер обязателен
+DATABASE_URL = settings.DATABASE_URL
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
 engine = create_async_engine(
-    settings.DATABASE_URL,
+    DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
 )
 
 async_session = async_sessionmaker(
     bind=engine,
+    class_=AsyncSession,
     expire_on_commit=False,
 )
