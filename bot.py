2a526c6537701aaa43fb6364b1d7b99119a30662
witheredwav <steam.akk1337@bk.ru>
2026-06-16 16:45:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,34 @@
 import asyncio
 
-from aiogram import Bot
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from database import create_tables
 
 from handlers.start import router as start_router
-
 from handlers.subscription import router as sub_router
-
 from handlers.codes import router as codes_router
-
 from handlers.admin import router as admin_router
 
 bot = Bot(token=BOT_TOKEN)
-
 dp = Dispatcher()
 
+
 async def on_startup():
-print("Бот запущен")
-await create_tables()
+    print("Бот запущен")
+    await create_tables()
+
 
 async def main():
+    await on_startup()
 
-await on_startup()
+    dp.include_router(start_router)
+    dp.include_router(sub_router)
+    dp.include_router(codes_router)
+    dp.include_router(admin_router)
 
-dp.include_router(start_router)
-dp.include_router(sub_router)
-dp.include_router(codes_router)
-dp.include_router(admin_router)
+    await dp.start_polling(bot)
 
 
-await dp.start_polling(bot)
-
-if **name** == "**main**":
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
