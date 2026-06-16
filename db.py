2a750aa7faa2e2730f9592ea6d0cb2979bab6654
witheredwav <steam.akk1337@bk.ru>
2026-06-16 18:12:07 +0500

    Update db.py

--- db.py
+++ db.py
@@ -22,12 +22,10 @@ async def init():
         await db.commit()
 
 
+# USERS
 async def add_user(tg_id):
     async with aiosqlite.connect(DB) as db:
-        await db.execute(
-            "INSERT OR IGNORE INTO users VALUES (?)",
-            (tg_id,)
-        )
+        await db.execute("INSERT OR IGNORE INTO users VALUES (?)", (tg_id,))
         await db.commit()
 
 
@@ -37,6 +35,7 @@ async def users_count():
         return (await cur.fetchone())[0]
 
 
+# CODES
 async def add_code(code, type_, content):
     async with aiosqlite.connect(DB) as db:
         await db.execute(
@@ -48,10 +47,7 @@ async def add_code(code, type_, content):
 
 async def get_code(code):
     async with aiosqlite.connect(DB) as db:
-        cur = await db.execute(
-            "SELECT * FROM codes WHERE code=?",
-            (code,)
-        )
+        cur = await db.execute("SELECT * FROM codes WHERE code=?", (code,))
         return await cur.fetchone()
 
 
